fe4136bace8fe8fc906266774b59ba6a2bbf4feb
jdequeant <jdequeant@gmail.com>
2013-09-04 16:24:30 +0200

    BUG date de naissance vide + Main

--- iCompService/src/fr/imie/main/Main.java
+++ iCompService/src/fr/imie/main/Main.java
@@ -7,9 +7,8 @@ import java.util.List;
 
 import fr.imie.dto.Utilisateur;
 
-
 public class Main {
-	//classe principale
+	// classe principale
 	private static ScreenName currentScreen = null;
 	public static Utilisateur currentUser = null;
 	public static List<Utilisateur> currentUsers = null;
@@ -203,18 +202,12 @@ public class Main {
 				break;
 
 			// UTILISATEURS PAR COMPETENCE
-				*/.metadata
-				*/RemoteSystemsTempFiles
 
 			case comp_users:
 
 				i = ScreenV2.competencesUser();
 
-				switch*/.metadata
-				*/RemoteSystemsTempFiles
- (i) {*/.metadata
-				*/RemoteSystemsTempFiles
-
+				switch (i) {
 
 				case 0:
 
